25d34550a1dba688fe405743060c63ee4620946e
“sla-zy” <mwangikadavid@gmail.com>
2023-12-06 12:21:52 -0800

    0x04. Python - More Data Structures: Set, Dictionary

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,16 @@
+Advance task
+vi 100-weight_average.py
+
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    num = 0
+    den = 0
+
+    for tup in my_list:
+        num += tup[0] * tup[1]
+        den += tup[1]
+
+    return (num / den)
